99da6ff7fedee9599d462c07a8c61e0bfd2d7b14
kensuke suzuki <35k.0101@gmail.com>
2021-05-13 20:58:40 +0900

    I18n国際化

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Rabbit
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     config.load_defaults 5.2
 
     Rails.application.config.assets.paths << Rails.root.join('vendor', 'javascripts')
