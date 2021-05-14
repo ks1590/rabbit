5bfd45f150d29a418f59fd595789f972196aa6ac
kensuke suzuki <35k.0101@gmail.com>
2021-05-14 21:23:45 +0900

    rspecセットアップ

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,6 @@ require_relative 'boot'
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Rabbit
@@ -14,9 +12,16 @@ module Rabbit
 
     Rails.application.config.assets.paths << Rails.root.join('vendor', 'javascripts')
     Rails.application.config.assets.paths << Rails.root.join('vendor', 'stylesheets')
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: false,
+        request_specs: false
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+    end
   end
 end
