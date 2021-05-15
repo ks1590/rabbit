6fac6589b53016a3d2a15c3fe69b0dbedde0b2eb
kensuke suzuki <35k.0101@gmail.com>
2021-05-15 21:06:25 +0900

    ユーザーのログイン判定追加

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -10,7 +10,7 @@ class TradesController < ApplicationController
   end
 
   def create
-    @trade = Trade.create(trade_params)
+    @trade = current_user.trades.build(trade_params)
     if @trade.save      
     else      
       respond_to do |format|        
