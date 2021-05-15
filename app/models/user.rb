89d7deefb2665020ee34fa47453a9a20bd96c0f0
kensuke suzuki <35k.0101@gmail.com>
2021-05-15 17:08:01 +0900

    userとtradeのアソシエーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+        :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true, length:{ maximum: 255 }
+  validates :email, presence: true, length:{ maximum: 255 },
+  format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  before_validation { email.downcase! }
+  validates :password, presence: true, length: { minimum: 6 }
+
+  has_many :trades, dependent: :destroy
 end
