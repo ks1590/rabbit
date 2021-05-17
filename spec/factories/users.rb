1d82607392717c089d940ce446afadda0debe351
kensuke suzuki <35k.0101@gmail.com>
2021-05-17 20:33:58 +0900

    ユーザー新規登録のテスト

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,13 @@ FactoryBot.define do
     name { 'user1' }
     email { 'user1@test.com' }
     password { "123456" }
+    password_confirmation { "123456" }
+  end
+
+  factory :second_user, class: User do
+    name { "user2" }
+    email { "user2@test.com" }
+    password { "123456" }
+    password_confirmation { "123456" }    
   end
 end
