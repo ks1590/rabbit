fd305518ba1c4264a4eeec90775676e3632159cc
kensuke suzuki <35k.0101@gmail.com>
2021-05-17 19:25:47 +0900

    userのバリデーションテスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
+RSpec.describe 'ユーザーモデル機能', type: :model do
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーションのテスト' do
+    
+    context 'メールアドレスが不正の値だった場合' do
+      it 'バリデーションにひっかかる' do
+        user = FactoryBot.build(:user, email: "12345")
+        expect(user).not_to be_valid
+      end
+    end
+
+    context 'パスワードが未入力の場合' do
+      it 'バリデーションにひっかかる' do
+        user = FactoryBot.build(:user, password: nil)
+        expect(user).not_to be_valid
+      end
+    end
+    
+    context 'メールアドレス、パスワードがある場合' do
+      it 'バリデーションが通る' do
+        user = FactoryBot.build(:user)
+        expect(user).to be_valid
+      end
+    end
+  end
 end
