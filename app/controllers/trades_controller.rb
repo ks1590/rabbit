f7c9cd2dd379fbaa40ddf8dc53d12387f5a8a8ab
kensuke suzuki <35k.0101@gmail.com>
2021-05-26 22:53:10 +0900

    WIP:積上げ棒グラフ実装の検証

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -1,8 +1,8 @@
 class TradesController < ApplicationController
   before_action :set_trade, only: [:edit, :update, :destroy]
 
-  def index
-    @trades = Trade.list_default(current_user)
+  def index    
+    @trades = current_user.trades.includes(:category)
   end
   
   def new
@@ -42,10 +42,8 @@ class TradesController < ApplicationController
   end
   
   def chart
-    @trades = Trade.list_default(current_user)
-    @expense_current_month = @trades.current_month.sum(:amount).to_i.to_s.gsub(/(\d)(?=(\d{3})+(?!\d))/, '\1,')   
-
-    months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    @trades = current_user.trades.includes(:category)
+    @expense_current_month = @trades.current_month.sum(:amount).to_i.to_s.gsub(/(\d)(?=(\d{3})+(?!\d))/, '\1,')
 
     hash_expense = {}
     arry_expense = Trade.current_month.sort_expense(current_user)
@@ -57,19 +55,19 @@ class TradesController < ApplicationController
     
     hash_expense.each do |data|
       data_expense.push(name: data[0], y: data[1])
-    end    
+    end
     
     @pie_this_month = LazyHighCharts::HighChart.new("graph") do |c|
       c.title(text: "今月の支出<br><br>#{@expense_current_month}円", align: "center", verticalAlign: "middle" )
       c.series({
         colorByPoint: true,
         data: data_expense
-      })      
+      })
       c.plotOptions(pie: {        
         innerSize: "80%",
         allowPointSelect: true,
         cursor: "pointer",
-        dataLabels: {          
+        dataLabels: {
           format: "{point.name}: {point.percentage:.1f} %",
           enabled: true, 
           distance: 0,
@@ -79,8 +77,8 @@ class TradesController < ApplicationController
             textAlign: "center", 
             textOutline: 0,
           }}
-        })   
-      c.chart(type: "pie")              
+        })
+      c.chart(type: "pie")
     end
     
     hash_expense = {}
@@ -102,23 +100,23 @@ class TradesController < ApplicationController
       c.series({
         colorByPoint: true,
         data: data_expense
-      })      
-      c.plotOptions(pie: {        
+      })
+      c.plotOptions(pie: {
         innerSize: "80%",
         allowPointSelect: true,
         cursor: "pointer",
-        dataLabels: {          
+        dataLabels: {     
           format: "{point.name}: {point.percentage:.1f} %",
           enabled: true, 
           distance: 0,
           allowOverlap: false,
           style: {
-            color: "#555",               
+            color: "#555",
             textAlign: "center", 
             textOutline: 0,
           }}
-        })   
-      c.chart(type: "pie")              
+        })
+      c.chart(type: "pie")
     end
 
     hash_expense = {}
@@ -140,7 +138,7 @@ class TradesController < ApplicationController
       c.series({
         colorByPoint: true,
         data: data_expense
-      })      
+      })
       c.plotOptions(pie: {        
         innerSize: "80%",
         allowPointSelect: true,
@@ -160,12 +158,34 @@ class TradesController < ApplicationController
       c.chart(type: "pie")              
     end
 
+    months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    expense_months = [Trade.month_jan, Trade.month_feb, Trade.month_mar, Trade.month_apr, Trade.month_may]
+    arr = []
+    @bar = LazyHighCharts::HighChart.new('column') do |f|
+      f.xAxis(categories: months, title: {text: '月'})
+      f.yAxis(:stackLabels=>{:enabled=> true},title: {text: '円'})
+
+      for m in expense_months do
+        arr.push(m.where(category_id: 1).sum(:amount))
+      end
+      
+      f.series(:name=>'食費',:data=> arr)
+      # f.series(:name=>'通信費',:data=>[100, 30, 400, 130, 230, 530, 340, 46,300,300,300,3000 ])
+      # f.series(:name=>'交通費',:data=> [3300, 2020, 3220, 5110, 4230, 1302, 1122,3200,3500,300,300,300 ])
+      # f.series(:name=>'ファッション',:data=> [3000, 1500, 1220, 4110, 2230, 902, 1522,1200,3100,300,300,300 ])
+      # f.series(:name=>'美容',:data=> [3300, 2020, 3220, 5110, 4230, 1302, 1122,3200,300,300,3500,3900 ])
+      f.title({ :text=>"月ごとの支出"})
+      f.options[:chart][:defaultSeriesType] = "column"
+      f.plot_options({:column=>{:stacking=>"normal"},:series=>{:dataLabels=>{:enabled=> true}}})
+      f.chart(type: "column",height: 550)
+    end
+
     @chart_globals = LazyHighCharts::HighChartGlobals.new do |f|
       f.global(useUTC: false)
       f.lang(thousandsSep: ",",
-      numericSymbols: nil)      
+      numericSymbols: nil)
       f.colors(["#FF9202", "#FF91AF", "#88CED3", "#393C99", "#3F62C4", "#4587CA", "#B8D808", "#78B45C", "#FFE053"])
-    end    
+    end
   end
 
   private
--- app/models/trade.rb
+++ app/models/trade.rb
@@ -10,9 +10,42 @@ class Trade < ApplicationRecord
   current_month = Date.today
   current_year = Date.today.all_year
 
+  month_jan = Date.today.beginning_of_year
+  month_feb = month_jan.since(1.months)
+  month_mar = month_jan.since(2.months)
+  month_apr = month_jan.since(3.months)
+  month_may = month_jan.since(4.months)
+  month_jun = month_jan.since(5.months)
+  month_jul = month_jan.since(6.months)
+  month_aug = month_jan.since(7.months)
+  month_sep = month_jan.since(8.months)
+  month_oct = month_jan.since(9.months)
+  month_nov = month_jan.since(10.months)
+  month_dec = month_jan.since(11.months)
+  
   scope :last_month, -> { where(event_on: last_month.beginning_of_month..last_month.end_of_month) }
   scope :current_month, -> { where(event_on: current_month.beginning_of_month..current_month.end_of_month) }
   scope :current_year, -> { where(event_on: current_year) }
+  scope :month_jan, -> { where(event_on: month_jan.beginning_of_month..month_jan.end_of_month) }
+  scope :month_feb, -> { where(event_on: month_feb.beginning_of_month..month_feb.end_of_month) }
+  scope :month_mar, -> { where(event_on: month_mar.beginning_of_month..month_mar.end_of_month) }
+  scope :month_apr, -> { where(event_on: month_apr.beginning_of_month..month_apr.end_of_month) }
+  scope :month_may, -> { where(event_on: month_may.beginning_of_month..month_may.end_of_month) }
+
+  def test_sum
+    months = [Trade.month_apr,Trade.month_may]
+    arr = []
+    joins(:category).pluck(:category_id, :name).uniq.sort 
+
+    for item in arry_expense do
+      hash_expense[item[1]] = Trade.current_month.where(category_id: item[0]).sum(:amount)
+    end
+    
+    hash_expense.each do |data|
+      arr.push(name: data[0], data: data[1])
+    end
+  end
+  
 
   def self.list_default(owner)
     joins(:payment).joins(:category).where(user_id: owner.id).order(event_on: :desc) 
